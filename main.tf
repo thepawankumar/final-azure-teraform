f789db57e30fa31c99bcbfd10cf8a29dddccc4d9
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-11-30 11:03:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ provider "azurerm" {
 
 resource "azurerm_container_registry" "acr" {
   name                = "containerRegistry5434565431"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = false
 }
