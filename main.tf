52cde5ac0cc97306f945c7b4787635956743dcdb
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-11-30 10:22:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,11 +25,35 @@ provider "azurerm" {
 
 
 # Create a resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "demo-rg"
+#resource "azurerm_resource_group" "rg" {
+ # name     = "demo-rg"
+  #location = "West Europe"
+  #tags =  {
+   # env = "dev"
+    #cost = "TA102AXY"
+  #}
+#}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "West Europe"
-  tags =  {
-    env = "dev"
-    cost = "TA102AXY"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "basic"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "North Europe"
+    zone_redundancy_enabled = true
+    tags                    = {}
   }
 }
+
