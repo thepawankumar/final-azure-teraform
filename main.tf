f5847360ab2bca2ec20a3972b38e5564b6ba7bbb
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-11-30 10:38:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,16 +43,6 @@ resource "azurerm_container_registry" "acr" {
   name                = "containerRegistry1"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  sku                 = "Premium"
+  sku                 = "basic"
   admin_enabled       = false
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-}
+ }
