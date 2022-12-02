4ed8232cb0f157bd3bf18d1df2c1187acfce1825
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-12-02 13:07:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,40 @@ terraform {
       version = "=2.46.0"
     }
   }
+
+# backend "azurerm" {
+ #   resource_group_name  = "friday-demo-rg"
+  #  storage_account_name = "sttfstatemgt01"
+   # container_name       = "tfstate"
+   # key                  = "demo.terraform.tfstate"
+ # }
+ }
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
 }
 
-resource "azurerm_resource_group" "acr_resource_group" {
-  name     = "${var.name}-rg"
-  location = var.location
+
+# Create a resource group
+  resource "azurerm_resource_group" "rg" {
+  name     = "rg"
+  location = "West Europe"
+  tags =  {
+  env = "dev"
+  cost = "TA102AXY"
+  }
 }
+
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.name}acr"
-  resource_group_name = azurerm_resource_group.acr_resource_group.name
-  location            = azurerm_resource_group.acr_resource_group.location
+  name                = "containerRegistry543456"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
   admin_enabled       = true
 }
