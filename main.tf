699011fb03daddaaa0cf42a9ac10eb95a7b2273e
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-12-02 12:57:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,26 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
+  backend "azurerm" {
+    resource_group_name  = "devopstamops-rg"
+    storage_account_name = "devopstamopssa"
+    container_name       = "tfstate"
+    key                  = "acr-terraform.tfstate"
   }
 }
-resource "azurerm_resource_group" "rg"  {
-  name     = "example-resources"
-   location = "West Europe"
+
+provider "azurerm" {
+    version = "~> 2.0"
+    features {}
 }
 
-resource  "azurerm_container_registry" "acr" {
-   name                     =  "containerRegistry1"
-   resource_group_name      =  azurerm_resource_group.rg.name
-   location                 =  azurerm_resource_group.rg.location
-  sku                       = "Standard"
-   admin_enabled            = true
+resource "azurerm_resource_group" "acr_resource_group" {
+  name     = "${var.name}-rg"
+  location = var.location
 }
 
-
-
-
-    resource "null_resource" "docker_push"  {
-      provisioner "local-exec" {
-       command = <<-EOT
-        docker  login  ${azurerm_container_registry.acr.login_server}  
-        docker push  ${azurerm_container_registry.acr.login_server}
-       EOT
-      }
-    }
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.name}acr"
+  resource_group_name = azurerm_resource_group.acr_resource_group.name
+  location            = azurerm_resource_group.acr_resource_group.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
