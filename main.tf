493dcedab2bcf72ff47a986ad95d2513269e6950
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-12-02 11:27:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,25 +19,8 @@ resource  "azurerm_container_registry" "acr" {
    admin_enabled            = true
 }
 
-resource  "azurerm_azuread_application" "acr-app"  {
-  name = "acr-app"
-}
 
-resource  "azurerm_azuread_service_principal"  "acr-sp" {
-  application_id =  "${azurerm_azuread_application.acr-app.application_id}"
-}
 
-resource  "azurerm_azuread_service_principal_password"  "acr-sp-pass" {
-  service_principal_id =  "${azurerm_azuread_service_principal.acr-sp.id}"
-   value                = "Password12345"
-   end_date             =  "2024-01-01T01:02:03Z"
-}
-
-resource  "azurerm_role_assignment"  "acr-assignment" {
-  scope                 =  "${azurerm_container_registry.acr.id}"
-   role_definition_name = "Contributor"
-   principal_id         =  "${azurerm_azuread_service_principal_password.acr-sp-pass.service_principal_id}"
-}
 
     resource "null_resource" "docker_push"  {
       provisioner "local-exec" {
