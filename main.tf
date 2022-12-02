942322d3e5ef03fe78a6607099683b478f7b3814
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-12-02 11:22:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 resource "azurerm_resource_group" "rg"  {
   name     = "example-resources"
    location = "West Europe"
