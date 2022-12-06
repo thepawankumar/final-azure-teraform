061cfb247b6a25849386a31acc292704a70dc375
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-12-06 10:39:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,46 @@
-variable "name" {
-  type        = string
-  default     = "devopsthehardway"
-  description = "Name for resources"
+variable "container_group_name" {
+  default       = "myContGroup"
+  description   = "The name of the container group"
+}
+
+variable "resource_group_name" {
+  default       = "MyContGroup-RG01"
+  description   = "The name of the resource group"
 }
 
 variable "location" {
-  type        = string
-  default     = "uksouth"
-  description = "Azure Location of resources"
+  default       = "westus"  
+  description   = "Azure location"
+}
+
+variable "dns_name_label" {
+  description   = "The DNS label/name for the container groups IP"
+}
+
+variable "os_type" {
+  description   = "The OS for the container group. Allowed values are Linux and Windows"
+}
+
+variable "container_name" {
+  default       = "mycont01"  
+  description   = "The name of the container"
+}
+
+variable "image_name" {
+  description   = "The container image name"
+}
+
+variable "cpu_core_number" {
+  default       = "0.5"  
+  description   = "The required number of CPU cores of the containers"
+}
+
+variable "memory_size" {
+  default       = "1.5"  
+  description   = "The required memory of the containers in GB"
+}
+
+variable "port_number" {
+  default       = "80"  
+  description   = "A public port for the container"
 }
