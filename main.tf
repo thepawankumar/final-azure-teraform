80d52b25b67e3452e8b6360a5b5be2b03d2c2bde
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-12-06 17:20:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "example-resources123"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_container_group" "example" {
   name                = "example-continst"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   ip_address_type     = "Public"
   dns_name_label      = "aci-label"
   os_type             = "Linux"
